40b7a7ccf07ef5b3c801dda26149a616e50d6b31
Patricio Tapia <native.devil@gmail.com>
2022-03-12 08:23:12 -0300

    ADD: Seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Create test containers and items:
+p 'Start Seeding!'
+container = Container.create(description: 'Test container from seeds file')
+item_1 = Item.create(description: 'First test item from seeds file')
+item_2 = Item.create(description: 'Second test item from seeds file')
+Content.create(container_id: container.id, item_id: item_1.id, quantity: 100)
+Content.create(container_id: container.id, item_id: item_2.id, quantity: 50)
+
+
+p 'Seed process over'
