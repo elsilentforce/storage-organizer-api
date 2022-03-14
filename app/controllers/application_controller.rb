8d3e7eb39874e8ae47bc4a3e2a9cd250cff7e8ca
Patricio Tapia <native.devil@gmail.com>
2022-03-14 00:06:18 -0300

    ADD: qr_as_base64 image method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::API
+
+  def qr_as_base64(url)
+    qrcode = RQRCode::QRCode.new(url)
+    png = qrcode.as_png(
+      resize_gte_to: false,
+      resize_exactly_to: false,
+      fill: 'white',
+      color: 'black',
+      size: 240,
+      border_modules: 4,
+      module_px_size: 6,
+    )
+    png.to_data_url
+  end
+
 end
