7e1aae737a33aa9bc5d6e02aa618bee7260bc144
Patricio Tapia <native.devil@gmail.com>
2022-03-14 00:11:38 -0300

    REFACT: Routes according to controllers

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,17 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      scope :items do
+        get '/:id', to: 'items#show', as: 'item_show'
+      end
+
+      scope :containers do
+        get '/:id', to: 'containers#show', as: 'container_show'
+        get '/:id/items', to: 'containers#items', as: 'container_items'
+      end
+
+    end
+  end
 end
