2f7ff732ba7ef28f9722ee678b392e2c2505a3b4
Patricio Tapia <native.devil@gmail.com>
2022-07-23 21:44:38 -0400

    ADD: Authorize request method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,22 @@
 class ApplicationController < ActionController::API
 
+  def not_found
+    render json: { error: 'not_found' }, status: 404
+  end
+
+  def authorize_request
+    header = request.headers['Authorization']
+    header = request.split(' ').last if header.nil?
+    begin
+      @decoded = JsonWebToken.decode(header)
+      @current_user = User.find(@decoded[:user_id])
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { errors: e.message }, status: :unauthorized
+    rescue JWT::DecodeError => e
+      render json: { errors: e.message }, status: :unauthorized
+    end
+  end
+
   def qr_as_base64(url)
     qrcode = RQRCode::QRCode.new(url)
     png = qrcode.as_png(
