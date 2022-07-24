c7f6129353869583ec8f85f70623d1c26be2258e
Patricio Tapia <native.devil@gmail.com>
2022-07-23 21:39:49 -0400

    REFACT: JWT Generation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :first_name, :last_name, presence: true
+
+  def generate_jwt
+    JWT.encode(
+      {
+        id: self.id,
+        exp: 30.days.from_now.to_i
+      },
+      Rails.application.secrets.secret_key_base.to_s
+    )
+  end
 end
